b66b5d578cd92b85ab7f55185ceb43a14505e457
Michael Wang <michael.secure.1@icloud.com>
2021-03-02 00:48:51 -0800

    Updates to login menu and global navbar

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -6,7 +6,10 @@ import { first } from 'rxjs/operators';
 import { AccountService, AlertService } from '@app/_services';
 import { Role } from '@app/_models';
 
-@Component({ templateUrl: './login.component.html' })
+@Component({ 
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
 
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
