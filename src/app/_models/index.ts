696d2efa5cd6121b2bf8948960ec4a0325988d21
Michael Wang <michael.secure.1@icloud.com>
2021-03-06 02:43:41 -0800

    Installation of Angular Material and overhaul of listing creation for Engage

--- src/app/_models/index.ts
+++ src/app/_models/index.ts
@@ -2,3 +2,4 @@ export * from './account';
 export * from './slate';
 export * from './alert';
 export * from './role';
+export * from './subject';
--- src/app/tutor/listings/listings.module.ts
+++ src/app/tutor/listings/listings.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTimepickerModule } from 'mat-timepicker';
 
 import { ListingsRoutingModule } from './listings-routing.module';
 import { OverviewComponent } from './overview/overview.component';
@@ -21,7 +28,14 @@ import { EditComponent } from './edit/edit.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ListingsRoutingModule
+    ListingsRoutingModule,
+    MatMomentDateModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatTimepickerModule
   ]
 })
 export class ListingsModule { }
