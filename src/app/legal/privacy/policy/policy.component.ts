e3d69baa243e8f205625b8151f760803a66f1cae
Michael Wang <michael.secure.1@icloud.com>
2021-03-09 20:20:52 -0800

    Migration from css to sass

--- src/app/legal/privacy/policy/policy.component.ts
+++ src/app/legal/privacy/policy/policy.component.ts
@@ -4,7 +4,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-policy',
   templateUrl: './policy.component.html',
-  styleUrls: ['./policy.component.css']
+  styleUrls: ['./policy.component.scss']
 })
 export class PolicyComponent implements OnInit {
   panelOpenState = false;
