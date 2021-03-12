77ccb1959c86550bca16a2a24286f7a2289d4dee
Michael Wang <michael.secure.1@icloud.com>
2021-03-11 21:11:31 -0800

    Resolved issue with listing search and return

--- src/app/tutor/listings/overview/overview.component.ts
+++ src/app/tutor/listings/overview/overview.component.ts
@@ -7,28 +7,31 @@ import { SlateService } from '@app/_services';
 @Component({ templateUrl: './overview.component.html' })
 
 export class OverviewComponent implements OnInit {
-  listings: any[];
-
-  constructor(private slateService: SlateService, private title: Title) { }
-
-  ngOnInit() {
-    this.title.setTitle('My Listing Overview')
-
-    this.slateService.getMyListings()
-      .pipe(first())
-      .subscribe(listings => this.listings = listings);
-  }
-
-  deleteListing(id: string) {
-    const listing = this.listings.find(x => x.id === id);
-    if (confirm(`Are you sure you want to delete this listing? This action cannot be reversed.`)) {
-      listing.isDeleting = true;
-      this.slateService.delete(id)
-        .pipe(first())
-        .subscribe(() => {
-          this.listings = this.listings.filter(x => x.id !== id);
-        });
-    }
-  }
-
+	listings: any[];
+	
+	constructor(
+		private slateService: SlateService,
+		private title: Title
+	) { }
+		
+	ngOnInit() {
+		this.title.setTitle('My Listing Overview')
+		
+		this.slateService.getMyListings()
+		.pipe(first())
+		.subscribe(listings => this.listings = listings);
+	}
+	
+	deleteListing(id: string) {
+		const listing = this.listings.find(x => x.id === id);
+		if (confirm(`Are you sure you want to delete this listing? This action cannot be reversed.`)) {
+			listing.isDeleting = true;
+			this.slateService.delete(id)
+			.pipe(first())
+			.subscribe(() => {
+				this.listings = this.listings.filter(x => x.id !== id);
+			});
+		}
+	}	
 }
+	
