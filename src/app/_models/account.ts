60ad89c3ce2c89bb20a3ee13e65e1e15e31e39e3
Michael Wang <michael.secure.1@icloud.com>
2021-03-19 22:43:08 -0700

    Added ratings to account model

--- src/app/_models/account.ts
+++ src/app/_models/account.ts
@@ -6,5 +6,12 @@ export class Account {
   lastName: string;
   email: string;
   role: Role;
+  behaviourRating: number;
+  overallContentRating: number;
+  mathContentRating: number;
+  scienceContentRating: number;
+  socialStudiesContentRating: number;
+  languageArtsContentRating: number;
+  ForeignLanguageAcquisitionContentRating: number;
   jwtToken?: string;
 }
