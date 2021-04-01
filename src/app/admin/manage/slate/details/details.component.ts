23157a181a71c65557b0e3f86afda2546063e258
Michael Wang <michael.secure.1@icloud.com>
2021-03-31 20:37:05 -0700

    Updated details component to use supplied user details

--- src/app/admin/manage/slate/details/details.component.ts
+++ src/app/admin/manage/slate/details/details.component.ts
@@ -26,51 +26,39 @@ export class DetailsComponent implements OnInit {
 		private route: ActivatedRoute,
 		private router: Router,
 		private snackBar: MatSnackBar,
-		private accountService: AccountService, // Ignore "unused" warnings
 		private slateService: SlateService,
 		private title: Title
-		) { }
+	) { }
+	
+	ngOnInit(): void {
+		this.title.setTitle('Slate Details');
 		
-		ngOnInit(): void {
-			this.title.setTitle('Slate Details');
+		this.id = this.route.snapshot.params['id'];
+		
+		this.slateService.getSlateById(this.id)
+		.pipe(first())
+		.subscribe(slate => {
+			this.isoCreated = slate.created;
+			this.isoUpdated = slate.updated;
+			slate.created = moment(slate.created).format('LT MMMM Do[,] YYYY');
+			slate.updated = moment(slate.updated).format('LT MMMM Do[,] YYYY');
 			
-			this.id = this.route.snapshot.params['id'];
+			this.isoStartDateTime = slate.startDateTime;
+			this.isoEndDateTime = slate.endDateTime;
+			slate.startDateTime = moment(slate.startDateTime).format('LT MMMM Do[,] YYYY');
+			slate.endDateTime = moment(slate.endDateTime).format('LT MMMM Do[,] YYYY');
 			
-			this.slateService.getSlateById(this.id)
-			.pipe(first())
-			.subscribe(slate => {
-				this.isoCreated = slate.created;
-				this.isoUpdated = slate.updated;
-				slate.created = moment(slate.created).format('LT MMMM Do[,] YYYY');
-				slate.updated = moment(slate.updated).format('LT MMMM Do[,] YYYY');
-				
-				this.isoStartDateTime = slate.startDateTime;
-				this.isoEndDateTime = slate.endDateTime;
-				slate.startDateTime = moment(slate.startDateTime).format('LT MMMM Do[,] YYYY');
-				slate.endDateTime = moment(slate.endDateTime).format('LT MMMM Do[,] YYYY');
-				
+			if (!!slate.registered) {
 				this.isoRegisterDate = slate.registerDate;
 				slate.registerDate = moment(slate.registerDate).format('LT MMMM Do[,] YYYY');
-
+			}
+			
+			if (slate.deleted) {
 				this.isoDeleteDate = slate.deleteDate;
 				slate.deleteDate = moment(slate.deleteDate).format('LT MMMM Do[,] YYYY');
-
-				this.accountService.getById(slate.account)
-				.pipe(first())
-				.subscribe(account => {
-					slate.tutorName = [account.firstName, account.lastName].join(' ');
-				});
-				
-				if (!!slate.registered) {
-					this.accountService.getById(slate.registered)
-					.pipe(first())
-					.subscribe(account => {
-						slate.studentName = [account.firstName, account.lastName].join(' ');
-					});
-				}
-				
-				this.slate = slate;
-			});
-		}
+			};
+			
+			this.slate = slate;
+		});
 	}
-	
+}
