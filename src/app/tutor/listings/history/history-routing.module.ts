1b73bb9cfbffcdf0cd5830e1e6a5e327f3e81cd1
Michael Wang <michael.secure.1@icloud.com>
2021-05-19 14:54:01 -0700

    Fixed formatting issue with tutor past sessions page

--- src/app/tutor/listings/history/history-routing.module.ts
+++ src/app/tutor/listings/history/history-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LayoutComponent } from './layout/layout.component';
 import { OverviewComponent } from './overview/overview.component';
 import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
-	{
-		path: '', component: LayoutComponent,
-		children: [
-			{ path: '', component: OverviewComponent },
-			{ path: 'details/:id', component: DetailsComponent }
-		]
-	}
+	{ path: '', component: OverviewComponent },
+	{ path: 'details/:id', component: DetailsComponent }
 ];
 
 @NgModule({
--- src/app/tutor/listings/history/history.module.ts
+++ src/app/tutor/listings/history/history.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HistoryRoutingModule } from './history-routing.module';
-import { LayoutComponent } from './layout/layout.component';
 import { OverviewComponent } from './overview/overview.component';
 import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent,
     OverviewComponent,
     DetailsComponent
   ],
