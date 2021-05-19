899641ec56dc762ccc15798888d5953e3f7e6743
Michael Wang <michael.secure.1@icloud.com>
2021-05-18 21:04:03 -0700

    Configured component for history

--- src/app/tutor/listings/history/history-routing.module.ts
+++ src/app/tutor/listings/history/history-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
 import { OverviewComponent } from './overview/overview.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
 	{
 		path: '', component: LayoutComponent,
 		children: [
-			{ path: '', component: OverviewComponent }
+			{ path: '', component: OverviewComponent },
+			{ path: 'details/:id', component: DetailsComponent }
 		]
 	}
 ];
--- src/app/tutor/listings/history/history.module.ts
+++ src/app/tutor/listings/history/history.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HistoryRoutingModule } from './history-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { OverviewComponent } from './overview/overview.component';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    OverviewComponent
+    OverviewComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule,
