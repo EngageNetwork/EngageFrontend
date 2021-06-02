13dd476f552a806675093a9b0f74c182a6364f41
Michael Wang <michael.secure.1@icloud.com>
2021-06-01 21:36:29 -0700

    Updates to global home component

--- src/app/global-home/global-home.component.ts
+++ src/app/global-home/global-home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
-@Component({ templateUrl: './global-home.component.html' })
+@Component({
+  templateUrl: './global-home.component.html',
+  styleUrls: ['./global-home.component.scss']
+})
 
 export class GlobalHomeComponent implements OnInit {
 
