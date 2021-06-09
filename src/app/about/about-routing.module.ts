1d351221b66fe018de3b886fd71f2c5dfb0db15e
Michael Wang <michael.secure.1@icloud.com>
2021-06-09 00:05:44 -0700

    Create most of overview for about page

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+import { OverviewComponent } from './overview/overview.component';
+
+const routes: Routes = [
+	{
+		path: '', component: LayoutComponent,
+		children: [
+			{ path: '', component: OverviewComponent },
+			// {
+			// 	path: 'sessions',
+			// 	loadChildren: () => import ('./sessions/sessions.module').then(x => x.SessionsModule)
+			// }
+		]
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class AboutRoutingModule { }
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
+import { LayoutComponent } from './layout/layout.component';
+import { OverviewComponent } from './overview/overview.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    OverviewComponent
+  ],
   imports: [
     CommonModule,
     AboutRoutingModule
