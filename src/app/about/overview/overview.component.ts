a44d21f7f5f385f867a3d30206cc64a2496433b3
Michael Wang <michael.secure.1@icloud.com>
2021-06-09 12:12:17 -0700

    Added title

--- src/app/about/overview/overview.component.ts
+++ src/app/about/overview/overview.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+	templateUrl: './overview.component.html',
+	styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	
+	constructor(private title: Title) { }
+	
+	ngOnInit() {
+		this.title.setTitle('About Us | Engage Network');
+	}
+	
 }
