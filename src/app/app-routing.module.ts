e13437a2ef263c6138f24de16d91acd5ed976245
Michael Wang <michael.secure.1@icloud.com>
2021-08-06 03:29:25 -0700

    Idea details and app requirements added to hackathon page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
 		canActivate: [DevGuard]
 	},
 	{
+		path: 'events',
+		loadChildren: () => import('./events/events.module').then(m => m.EventsModule),
+	},
+	{
 		path: 'legal',
 		loadChildren: () => import('./legal/legal.module').then(m => m.LegalModule)
 	},
